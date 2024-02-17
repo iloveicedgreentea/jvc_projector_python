a8b01ae442ace16f254674304d6283d822f64b6b
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-02-17 11:29:48 -0500

    simplify

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyjvc",
-    version="0.0.18",
+    version="0.1.0",
     author="iloveicedgreentea2",
     description="A package to control JVC projectors over IP",
     long_description=long_description,
--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -31,6 +31,7 @@ os.environ["LOG_LEVEL"] = "DEBUG"
 
 
 class TestCoordinator(unittest.IsolatedAsyncioTestCase):
+    """Test running commands"""
     async def asyncSetUp(self):
         # set up connection
         options = JVCInput(TEST_IP, TEST_PASSWORD, TEST_PORT, 5)
@@ -60,129 +61,139 @@ class TestCoordinator(unittest.IsolatedAsyncioTestCase):
         """clean up connection between tests otherwise error"""
         await self.coordinator.close_connection()
 
-    async def assert_modes(self): # pylint: disable=too-many-local-variables
+    async def assert_modes(self):  # pylint: disable=too-many-locals
         """helper function for attr"""
         # in one test so it doesnt constantly open and close connection
         # check return value against eunm __members__
         low_latency_state = await self.coordinator.get_low_latency_state()
-        assert (
-            low_latency_state in LowLatencyModes.__members__
-        ), f"Unexpected low latency state: {low_latency_state}"
+        self.assertIn(
+            low_latency_state,
+            LowLatencyModes.__members__,
+            f"Unexpected low latency state: {low_latency_state}",
+        )
 
         res = await self.coordinator.get_software_version()
-        assert isinstance(res, str)  # sw can change, but should be str
+        self.assertIsInstance(res, str)  # sw can change, but should be str
 
         picture_mode = await self.coordinator.get_picture_mode()
-        assert picture_mode in PictureModes.__members__, "Picture mode not as expected."
+        self.assertIn(
+            picture_mode, PictureModes.__members__, "Picture mode not as expected."
+        )
 
         install_mode = await self.coordinator.get_install_mode()
-        assert (
-            install_mode in InstallationModes.__members__
-        ), f"Unexpected install mode: {install_mode}"
+        self.assertIn(
+            install_mode,
+            InstallationModes.__members__,
+            f"Unexpected install mode: {install_mode}",
+        )
         # Testing input mode
 
         input_mode = await self.coordinator.get_input_mode()
-        assert (
-            input_mode in InputModes.__members__
-        ), f"Unexpected input mode: {input_mode}"
+        self.assertIn(
+            input_mode, InputModes.__members__, f"Unexpected input mode: {input_mode}"
+        )
 
         # Testing mask mode
         mask_mode = await self.coordinator.get_mask_mode()
-        assert mask_mode in MaskModes.__members__, f"Unexpected mask mode: {mask_mode}"
+        self.assertIn(
+            mask_mode, MaskModes.__members__, f"Unexpected mask mode: {mask_mode}"
+        )
 
         # Testing laser mode
         laser_mode = await self.coordinator.get_laser_mode()
-        assert (
-            laser_mode in LaserDimModes.__members__
-        ), f"Unexpected laser mode: {laser_mode}"
+        self.assertIn(
+            laser_mode,
+            LaserDimModes.__members__,
+            f"Unexpected laser mode: {laser_mode}",
+        )
 
         # Testing eshift mode
         eshift_mode = await self.coordinator.get_eshift_mode()
-        assert (
-            eshift_mode in EshiftModes.__members__
-        ), f"Unexpected eshift mode: {eshift_mode}"
+        self.assertIn(
+            eshift_mode,
+            EshiftModes.__members__,
+            f"Unexpected eshift mode: {eshift_mode}",
+        )
 
         # Testing color mode
         color_mode = await self.coordinator.get_color_mode()
-        assert (
-            color_mode in ColorSpaceModes.__members__
-        ), f"Unexpected color mode: {color_mode}"
+        self.assertIn(
+            color_mode,
+            ColorSpaceModes.__members__,
+            f"Unexpected color mode: {color_mode}",
+        )
 
         # Testing input level
         input_level = await self.coordinator.get_input_level()
-        assert (
-            input_level in InputLevel.__members__
-        ), f"Unexpected input level: {input_level}"
+        self.assertIn(
+            input_level,
+            InputLevel.__members__,
+            f"Unexpected input level: {input_level}",
+        )
 
         # Testing software version
         software_version = await self.coordinator.get_software_version()
-        assert isinstance(software_version, str), "Software version is not a string"
+        self.assertIsInstance(software_version, str, "Software version is not a string")
 
         # Testing content type
         content_type = await self.coordinator.get_content_type()
-        assert (
-            content_type in ContentTypes.__members__
-        ), f"Unexpected content type: {content_type}"
+        self.assertIn(
+            content_type,
+            ContentTypes.__members__,
+            f"Unexpected content type: {content_type}",
+        )
 
         # Testing content type transition
         content_type_trans = await self.coordinator.get_content_type_trans()
-        assert (
-            content_type_trans in ContentTypeTrans.__members__
-        ), f"Unexpected content type transition: {content_type_trans}"
-
-        # Cant test this without signal because again, JVC can't be bothered to return a failure these just time out
-        # # Testing HDR processing
-        # hdr_processing = await self.coordinator.get_hdr_processing()
-        # assert hdr_processing in HdrProcessing.__members__, f"Unexpected HDR processing: {hdr_processing}"
-
-        # # Testing HDR level
-        # hdr_level = await self.coordinator.get_hdr_level()
-        # assert hdr_level in HdrLevel.__members__, f"Unexpected HDR level: {hdr_level}"
-
-        # # Testing HDR data
-        # hdr_data = await self.coordinator.get_hdr_data()
-        # assert hdr_data in HdrData.__members__, f"Unexpected HDR data: {hdr_data}"
+        self.assertIn(
+            content_type_trans,
+            ContentTypeTrans.__members__,
+            f"Unexpected content type transition: {content_type_trans}",
+        )
 
         # Testing lamp power
         lamp_power = await self.coordinator.get_lamp_power()
         # unknown would mean it worked but returned an unmapped value, thats fine
-        assert (
-            lamp_power in LampPowerModes.__members__ or lamp_power == "unknown"
-        ), f"Unexpected lamp power: {lamp_power}"
+        self.assertTrue(
+            lamp_power in LampPowerModes.__members__ or lamp_power == "unknown",
+            f"Unexpected lamp power: {lamp_power}",
+        )
 
         # Testing lamp time
         lamp_time = await self.coordinator.get_lamp_time()
-        assert isinstance(lamp_time, int), "Lamp time is not an integer"
+        self.assertIsInstance(lamp_time, int, "Lamp time is not an integer")
 
         # Testing laser power
         laser_power = await self.coordinator.get_laser_power()
-        assert (
-            laser_power in LaserPowerModes.__members__
-        ), f"Unexpected laser power: {laser_power}"
-
-        # Testing theater optimizer state
-        # theater_optimizer_state = await self.coordinator.get_theater_optimizer_state()
-        # assert theater_optimizer_state in TheaterOptimizer.__members__, f"Unexpected theater optimizer state: {theater_optimizer_state}"
+        self.assertIn(
+            laser_power,
+            LaserPowerModes.__members__,
+            f"Unexpected laser power: {laser_power}",
+        )
 
         # Testing aspect ratio
         aspect_ratio = await self.coordinator.get_aspect_ratio()
-        assert (
-            aspect_ratio in AspectRatioModes.__members__
-        ), f"Unexpected aspect ratio: {aspect_ratio}"
+        self.assertIn(
+            aspect_ratio,
+            AspectRatioModes.__members__,
+            f"Unexpected aspect ratio: {aspect_ratio}",
+        )
 
         # Testing source status
         source_status = await self.coordinator.get_source_status()
-        assert (
-            source_status in SourceStatuses.__members__
-        ), f"Unexpected source status: {source_status}"
+        self.assertIn(
+            source_status,
+            SourceStatuses.__members__,
+            f"Unexpected source status: {source_status}",
+        )
 
         # Testing is_on
         is_on = await self.coordinator.is_on()
-        assert isinstance(is_on, bool), "is_on is not a boolean"
+        self.assertIsInstance(is_on, bool, "is_on is not a boolean")
 
         # Testing is_ll_on
         is_ll_on = await self.coordinator.is_ll_on()
-        assert isinstance(is_ll_on, bool), "is_ll_on is not a boolean"
+        self.assertIsInstance(is_ll_on, bool, "is_ll_on is not a boolean")
 
     async def test_exec_commands(self):
         """test executing commands"""
